4372f508e0cc64b25d46968908d09e20c8c9d928
Tyson Hall <tn8938@gmail.com>
2023-10-22 16:30:59 -0400

    Added displayAllEntries() in Ledger.java

--- src/main/java/com/pluralsight/Ledger.java
+++ src/main/java/com/pluralsight/Ledger.java
@@ -1,8 +1,14 @@
 package com.pluralsight;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Ledger {
+
+    ArrayList<Transaction> allTransactions = new ArrayList<>();
+
     public void displayLedgerScreen () {
         Scanner scanner = new Scanner(System.in);
 
@@ -43,6 +49,43 @@ public class Ledger {
 
     private void displayAllEntries() {
         System.out.println("display All Entries");
+
+        try {
+            FileReader fileReader = new FileReader("trans.csv");
+            BufferedReader bufferedReader = new BufferedReader(fileReader);
+            String input;
+
+            // arraylist = {"date|time|description|vendor|amount", ... , }
+            // goal arraylist = {date|time|description|vendor|amount, ... , }
+
+            bufferedReader.readLine(); // reads the first line & moves on to the next line
+
+            while ((input = bufferedReader.readLine()) != null) {
+                String[] fields = (input.toString()).split("\\|");
+                // String = {"date", "time", "description", "vendor", "amount"};
+
+                var tDate = fields[0].trim();
+                var tTime = fields[1].trim();
+                var tDescription = fields[2].trim();
+                var tVendor = fields[3].trim();
+                var tAmount = Double.valueOf(fields[4]);
+                // var tAmount = Double.parseDouble(fields[4]);
+
+                Transaction t = new Transaction(tDate, tTime, tDescription, tVendor, tAmount);
+                System.out.printf(
+                        "\ntDate: %s, \ntTime: %s, \ntDescription: %s, \ntVendor: %s, \ntAmount: %.2f \n",
+                        t.getDate(), t.getTime(), t.getDescription(), t.getVendor(), t.getAmount()
+                );
+
+                // allTransactions.add(t);
+            }
+            // System.out.println(allTransactions);
+            System.out.println(); // to make a line space
+            bufferedReader.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
     }
 
     private void displayDeposits() {
